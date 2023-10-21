b0351e9dfdecd32ae969039c7dab5427ccfa5007
knull-a <kenjopa6@gmail.com>
2023-10-21 20:59:00 +0500

    add more unit tests

--- src/validateValue/validateValue.test.ts
+++ src/validateValue/validateValue.test.ts
@@ -1,5 +1,23 @@
 import { validateValue } from "./validateValue";
 
-test("Value validation", () => {
-  expect(validateValue(50)).toBe(true);
+describe("Value validation", () => {
+  test("Correct", () => {
+    expect(validateValue(50)).toBe(true);
+  });
+
+  test("Less than correct", () => {
+    expect(validateValue(-1)).toBe(false);
+  });
+
+  test("More than correct", () => {
+    expect(validateValue(101)).toBe(false);
+  });
+
+  test("Boundary value (minimum)", () => {
+    expect(validateValue(0)).toBe(true);
+  });
+
+  test("Boundary value (maximum)", () => {
+    expect(validateValue(100)).toBe(true);
+  });
 });
